5c8e993efcb2e321906d1ba741cb23fefb17de5e
Ajinkya Bhilare <ajinkya0011@gmail.com>
2022-08-25 10:47:55 +0530

    second

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const abcd  = () => {
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
+const abce  = () => {
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
+const abcdf  = () => {
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
+const abcdg = () => {
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
